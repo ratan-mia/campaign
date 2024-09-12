6258b1316462ae11d401f12443927d647d797518
Ratan Mia <ratan.kawasaki@gmail.com>
2024-09-12 13:44:51 +0600

    added google analytics

--- index.php
+++ index.php
@@ -62,6 +62,19 @@
             src="https://www.facebook.com/tr?id=538928328480185&ev=PageView&noscript=1" /></noscript>
     <!-- End Meta Pixel Code -->
 
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-8H130FQM5D"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+
+        function gtag() {
+            dataLayer.push(arguments);
+        }
+        gtag('js', new Date());
+
+        gtag('config', 'G-8H130FQM5D');
+    </script>
+
 </head>
 
 <body>
